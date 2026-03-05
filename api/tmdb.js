6bfbd362c106ac3af26ff8f1194749a77291685b
Kevin Li <kevjaredli@gmail.com>
2026-03-04 17:33:39 -0800

    changed key to token

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const query = req.url.split('query=')[1] || '';
-  const token = process.env.TMDB_KEY;
+  const token = process.env.TMDB_TOKEN;
 
   try {
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, {
